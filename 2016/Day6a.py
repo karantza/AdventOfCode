593c9afc471ad8d194788aa250dbb25ecb1fa2ad
Alex Karantza <akarantza@entradahealth.com>
2016-12-06 11:53:57 -0500

    Day 6 solutions

---
+++ 2016/Day6a.py
@@ -0,0 +1,12 @@
+
+with open('Day6.txt', 'r') as file:
+	input = file.read().splitlines()
+
+	strlen = max([len(x) for x in input])
+
+	message = ""
+	for i in range(0, strlen):
+		char = [x[i] for x in input]
+		message += max(set(char), key=char.count)
+
+	print message
